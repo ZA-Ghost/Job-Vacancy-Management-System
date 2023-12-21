e16fbad8b40879ec869a3d78c25656a55e2c9a43
ZA_Ghost <119295603+ZA-Ghost@users.noreply.github.com>
2023-12-21 22:11:30 +0200

    Add files via upload

--- jobPoster/src/java/admin_job_poster.java
+++ jobPoster/src/java/admin_job_poster.java
@@ -21,35 +21,33 @@ public class admin_job_poster extends HttpServlet {
         
         
             String Date = request.getParameter("date");
-            String Job_Post = request.getParameter("job_description");
+            String Job_Post = request.getParameter("job_post");
             String Closing_Date = request.getParameter("closing_date");
-            
-          
+            String jobTitle = request.getParameter("jobTitle");
+            String jobStatus = request.getParameter("jobStatus");
+            String jobReference = request.getParameter("jobReference");
         
         try{
             Class.forName("com.mysql.jdbc.Driver"); //
              Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jobposter","root","");
  
-             String sql= "INSERT INTO job_post (date, job_post, closing_date) VALUES(?,?,?)";
+             String sql= "INSERT INTO job_post (date, jobTitle, jobReference, job_post, closing_date, jobStatus) VALUES(?,?,?,?,?,?)";
        
              
              PreparedStatement pst = connection.prepareStatement(sql);
              
              
              pst.setString(1,Date);
-             pst.setString(2,Job_Post);
-             pst.setString(3,Closing_Date);
+             pst.setString(2,jobTitle);
+             pst.setString(3,jobReference);
+             pst.setString(4,Job_Post);
+             pst.setString(5,Closing_Date);
+             pst.setString(6,jobStatus);
             
  
          
-           
-            if(Date.isEmpty() || Job_Post.isEmpty() || Closing_Date.isEmpty()){
-                  out.println("Details Have Not Been Captured !!!");         
-            }else{
-                pst.executeUpdate();
+           pst.executeUpdate();
                 response.sendRedirect("admin-job-post.jsp");
-             
-            } 
             } 
         catch(ClassNotFoundException | SQLException e){
             System.out.println(e.getMessage());
